0ad7bb881aada9fe82fa5c101a635506bc2d0af3
deepak__victor <deepakvictor7585@gmail.com>
2020-11-04 13:06:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import Todo from './components/todo main'
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [todos ,settodos] = useState([''])
+  const [input, setinput] = useState('')
+  function handler(Event){
+
+    settodos([...todos,input])
+    setinput('')
+    Event.preventDefault();
+  }
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo />
+      <form>
+      <input type="text" value= {input} onChange={Event => setinput(Event.target.value)}/>
+            <button type="submit" disabled= {!input} onClick = {handler}> add </button>
+            </form>
+          <ul class="list-group list-group-flush" >{ todos.map(todo => ( <li class="list-group-item" > {todo} </li>))} </ul> 
     </div>
   );
 }
---
+++ src/components/todo main.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'; 
+class Todo extends Component {
+    state = {  }
+    render() { 
+
+        return ( <div> 
+
+        <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <h3 class="breadcrumb-item active" aria-current="page">Todo app</h3>
+        </ol>
+        </nav>
+        
+           </div>);
+
+           
+    }
+}
+ 
+export default Todo;
